d15af74f0b953b0484dfd3dad8f3bf9086ce1a63
Drake Redfield <drake.redfield@hotmail.com>
2022-07-23 21:32:25 -0500

    feat: add seeds into db mongo folder

--- src/db/mongo/config/mongodb.service.ts
+++ src/db/mongo/config/mongodb.service.ts
@@ -4,6 +4,5 @@ export const conectoToMongoDB = () => {
   mongoose.connect(process.env.MONGODB_URI!, (error) => {
     if (error) throw error;
     console.log('[mongodb]: Connection Success')
-  }
-  );
+  });
 }
